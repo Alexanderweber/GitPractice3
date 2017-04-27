815395507e994952021c1a41bf4ee44702e0d498
Louis Weber <louis_apple@louiss-macbook-air.local>
2017-04-27 19:59:54 +0200

    Added new project

---
+++ GitPractice3/GitPractice3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitPractice3
+//
+//  Created by Alexander Weber on 2017/04/27.
+//  Copyright © 2017 Alexander Weber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
