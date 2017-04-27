112a7a52f45ad43907c7e24698abe001707250ff
Louis Weber <louis_apple@louiss-macbook-air.local>
2017-04-27 20:46:25 +0200

    new file

--- GitPractice3/GitPractice3/ViewController.swift
+++ GitPractice3/GitPractice3/ViewController.swift
@@ -12,14 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func testFunction(){
+        print("Test 1")
     }
-
-
+    func testFunction2(){
+        print("Test 2")
+    }
 }
 
